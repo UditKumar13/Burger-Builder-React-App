3fe8088826cad29ba2eb76d6692e695499096d4a
Uditkumar13 <uditkumar2401@gmail.com>
2020-06-08 19:48:44 +0530

    BurgerIngredient

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
-import classes from './Layout.css';
+import classes from '../Layout/Layout.css';
 
 const layout = (props) =>(
 <Aux>
