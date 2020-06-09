296fbcc975f7de52d68fb89dbd138a409d93adff
Uditkumar13 <uditkumar2401@gmail.com>
2020-06-09 10:07:41 +0530

    Burger on screen

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,8 +1,8 @@
-import React ,{Component} from 'react';
+import React , {Component} from 'react';
 import PropTypes from 'prop-types';
 import classes from './BurgerIngredient.css';
 
-class BurgerIngredient extends Component = (props)=> {
+class BurgerIngredient extends Component  {
 
     render(){
         let ingredient = null ;
@@ -41,10 +41,10 @@ class BurgerIngredient extends Component = (props)=> {
     
     }
    
-}
+};
 
-BurgerIngredient.PropTypes = {
-    type:PropTypes.string.isRequired
+BurgerIngredient.propTypes = {
+     type: PropTypes.string.isRequired
 };
 
 export default BurgerIngredient;
