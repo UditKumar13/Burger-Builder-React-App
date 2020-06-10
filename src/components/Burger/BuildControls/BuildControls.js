624dbf7e9e31050a0c90afc4225d1449a4f45984
Uditkumar13 <uditkumar2401@gmail.com>
2020-06-10 07:43:50 +0530

    adding disabled functioanlity

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,8 @@ const buildControls = (props) =>(
     <BuildControl key={ctrl.label}
      label={ctrl.label}
      added={()=>props.ingredientAdded(ctrl.type)}
-     removed={()=>props.ingredientRemoved(ctrl.type)}/>
+     removed={()=>props.ingredientRemoved(ctrl.type)}
+     disabled={props.disabled[ctrl.type]}/>
 ))}
 </div>
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -41,6 +41,9 @@ class BurgerBuilder extends Component{
 
     removeIngredientHandler=(type)=>{
         const oldCount = this.state.ingredients[type];
+        if(oldCount<=0){
+            return;
+        }
         const updatedCount =oldCount-1;
         const updatedIngredients = {
             ...this.state.ingredients
@@ -57,13 +60,23 @@ class BurgerBuilder extends Component{
     }
 
     render(){
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
 
+        for (let key in disabledInfo){
+            disabledInfo[key] = disabledInfo[key]<=0;
+
+          
+        }
+        
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                 ingredientAdded={this.addIngrdientHandler}
-                ingredientRemoved={this.removeIngredientHandler}/>
+                ingredientRemoved={this.removeIngredientHandler}
+                disabled={disabledInfo}/>
             </Aux>
         );
     }
