8b5d96fa8ae4c83abfe05cc2661192d306898b41
Uditkumar13 <uditkumar2401@gmail.com>
2020-06-12 18:34:06 +0530

    Reusing the backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
@@ -6,15 +6,30 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
 
-const layout = (props) =>(
-  
-<Aux>
-<Toolbar/>
-<SideDrawer/>
-<main className={classes.Layout_Content}>
-{props.children}
-</main>
-</Aux>
-);
+class  Layout extends Component{
 
-export default layout;
+    state={
+        showSideDrawer:true
+    }
+
+    sideDrawerClosedHandler=()=>{
+        this.setState({showSideDrawer:false})
+
+    }
+
+    render(){
+        return(
+                    <Aux>
+                    <Toolbar/>
+                    <SideDrawer closed={this.sideDrawerClosedHandler}
+                    open={this.state.showSideDrawer}/>
+                    <main className={classes.Layout_Content}>
+                    {this.props.children}
+                    </main>
+                    </Aux>
+        );
+    }
+
+};
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,11 +2,22 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../../Navigation/NavigationItems/NavigationItems';
 import classes from '../SideDrawer/SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 
 
 const sideDrawer = (props) =>{
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if(props.open){
+        attachedClasses=[classes.SideDrawer,classes.Open];
+    }
+
 return (
-    <div className={classes.SideDrawer}>
+    <Aux>
+      <Backdrop show={props.open} 
+      clicked={props.closed}/>  
+      <div className={attachedClasses.join(' ')}>
         <div className={classes.Logo}>
         <Logo/>
         </div>
@@ -14,6 +25,8 @@ return (
             <NavigationItems/>
         </nav>
     </div>
+    </Aux>
+  
 );
 };
 
