5d53481125a9e404f193089dbdd25ded253a15df
Uditkumar13 <uditkumar2401@gmail.com>
2020-06-27 17:38:56 +0530

    post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-order';
 
 const INGREDIENT_PRICES = {
     salad:0.5,
@@ -94,7 +95,27 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler=()=>{
-    alert('You Control !');
+
+    const order = {
+        ingredients : this.state.ingredients,
+        price:this.state.totalPrice,
+        customer : {
+            name:'udit kumar',
+            address : {
+                street : 'Newyork street',
+                zipcode:'41351',
+                country : 'India'
+            },
+
+            email:'uk@test.com'
+        },
+        deliveryMethod:'fastest'
+        }
+    
+    axios.post('/orders.json',order)
+    .then(response=>console.log(response))
+    .catch (error => console .log (error));
+
     }
     render(){
         const disabledInfo = {
