aa636f530fcd06807727f171ab69bd32612f557a
Uditkumar13 <uditkumar2401@gmail.com>
2020-06-28 12:01:37 +0530

    error handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-order';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad:0.5,
@@ -116,7 +117,7 @@ class BurgerBuilder extends Component{
         deliveryMethod:'fastest'
         }
     
-    axios.post('/orders.json',order)
+    axios.post('/orders',order)
     .then(response=>{
         this.setState({loading:false,purchasing:false});
     })
@@ -163,4 +164,4 @@ class BurgerBuilder extends Component{
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder,axios);
