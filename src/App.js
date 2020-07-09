84faf8ab89b2d2e28bd62196b0cd159f12227a38
Uditkumar13 <uditkumar2401@gmail.com>
2020-07-10 00:30:20 +0530

    Setting up the Routing and Router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Layout from '../src/hoc/Layout/Layout';
 import '../src/index.css';
 import BurgerBuilder from '../src//containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import {Route,Switch} from 'react-router-dom';
+
 
 class App extends Component {
 
@@ -13,8 +15,10 @@ class App extends Component {
   return (
     <div>
       <Layout>
-      <BurgerBuilder/>
-      <Checkout/>
+      <Switch>
+      <Route path="/checkout" component={Checkout}></Route>
+        <Route path="/" exact component={BurgerBuilder}></Route>
+      </Switch>
       </Layout>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
+const app = (
+  <BrowserRouter>
+     <App />
+ </BrowserRouter>
+
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    app
+  ,
   document.getElementById('root')
 );
 
