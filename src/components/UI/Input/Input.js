a27d75bf4b39d0b7cb635d8453989c160f769994
Uditkumar13 <uditkumar2401@gmail.com>
2020-07-14 22:54:46 +0530

    Changing the color

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,13 +5,13 @@ const input  = (props) =>{
 let inputElement = null;
 switch(props.inputtype){
     case('input'):
-    inputElement=<input className={classes.inputElement} {...props}/>
+    inputElement=<input className={classes.InputElement} {...props}/>
     break;
     case('textarea'):
-    inputElement=<textarea className={classes.inputElement} {...props}/>
+    inputElement=<textarea className={classes.InputElement} {...props}/>
     break;
     default:
-        inputElement=<input className={classes.inputElement} {...props}/>
+        inputElement=<input className={classes.InputElement} {...props}/>
     
 
 }
