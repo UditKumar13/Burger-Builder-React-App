44092c0817f172c637d9fda1a887e33f983e1d59
Uditkumar13 <uditkumar2401@gmail.com>
2020-07-25 22:52:40 +0530

    Initial Redux and React Redux

---
+++ src/Store/reducer.js
@@ -0,0 +1,11 @@
+import * as actionsTypes from './actions';
+const initialState  = {
+    ingredients:null,
+    totalPrice:4
+};
+
+const reducer = (state = initial state, action) => {
+
+};
+
+export default reducer;
