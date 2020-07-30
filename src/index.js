8a112186ee877534f647542e0f3937a989bdd58e
Uditkumar13 <uditkumar2401@gmail.com>
2020-07-30 20:35:09 +0530

    Executing Asynchronous code

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore,applyMiddleware,compose} from 'redux';
 import burgerBuilderReducer from './Store/reducers/burgerBuilder';
-
+import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
-
-const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(burgerBuilderReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 
 const app = (
