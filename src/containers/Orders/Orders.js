cfe9b5d92aeeb932141dee28db378877f2bc90eb
Uditkumar13 <uditkumar2401@gmail.com>
2020-08-05 07:02:10 +0530

    Accessing protected resources

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,7 +9,7 @@ class Orders extends Component {
     
     componentDidMount(){
       
-       this.props.onFetchOrders();
+       this.props.onFetchOrders(this.props.token);
     }
     render(){let orders=<Spinner/>;
         if(!this.props.loading){
@@ -36,13 +36,14 @@ class Orders extends Component {
 const mapStateToProps = state=>{
     return{
         orders:state.order.orders,
-        loading:state.order.loading
+        loading:state.order.loading,
+        token:state.auth.token
     };
 }
 
 const mapDispatchToProps = dispatch =>{
     return {
-        onFetchOrders:()=>dispatch(actions.fetchOrders())
+        onFetchOrders:(token)=>dispatch(actions.fetchOrders(token))
     };
 }
 export default connect(mapStateToProps,mapDispatchToProps)(withErrorHandler(Orders,axios));
