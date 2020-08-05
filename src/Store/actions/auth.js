9184c010fa286a4e8ec261dd0c043bf551aa3bc4
Uditkumar13 <uditkumar2401@gmail.com>
2020-08-05 06:16:32 +0530

    Adding a spinner

--- src/Store/actions/auth.js
+++ src/Store/actions/auth.js
@@ -23,6 +23,21 @@ export const authFail  = (error) =>{
     };
 };
 
+export const logout=() =>{
+return{
+    type:actionTypes.AUTH_LOGOUT
+};
+};
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch=>{
+        setTimeout(()=>{
+            dispatch(logout());
+        },expirationTime);
+
+    };
+
+};
+
 export const auth = (email,password,isSignup)=>{
     return dispatch=> {
         dispatch(authStart());
@@ -38,7 +53,7 @@ export const auth = (email,password,isSignup)=>{
         axios.post(url,authData)  
         .then(response=>{console.log(response);
             dispatch(authSuccess(response.data.idToken,response.data.localId));
-
+            dispatch(checkAuthTimeout(response.data.expiresIn)) ;
         })
         .catch(err=>{
             //console.log(err);
