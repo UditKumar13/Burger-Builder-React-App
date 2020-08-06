c0f5af193194b79b34facf25afc160b835ffa329
Uditkumar13 <uditkumar2401@gmail.com>
2020-08-06 23:29:09 +0530

    Displaying user specific orders

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Layout from '../src/hoc/Layout/Layout';
 import '../src/index.css';
 import BurgerBuilder from '../src//containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import {Route,Switch,withRouter} from 'react-router-dom';
+import {Route,Switch,withRouter,Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
@@ -19,25 +19,43 @@ class App extends Component {
   }
 
   render(){
+    let routes=(
+      <Switch>
+      <Route path="/auth" component={Auth}></Route>
+      <Route path="/" exact component={BurgerBuilder}></Route>
+      <Redirect to="/" />
+      </Switch>
+    );
+    if (this.props.isAuthenticated){
+      routes=(
+        <Switch>
+          <Route path="/checkout" component={Checkout}></Route>
+      <Route path="/orders" component={Orders}></Route>
+      <Route path="/logout" component={Logout}></Route>
+      <Route path="/" exact component={BurgerBuilder}></Route>
+      <Redirect to="/" />  
+      </Switch>
+      );
+    }
   return (
     <div>
       <Layout>
-      <Switch>
-      <Route path="/checkout" component={Checkout}></Route>
-      <Route path="/orders" component={Orders}></Route>
-      <Route path="/auth" component={Auth}></Route>
-      <Route path="/logout" component={Logout}></Route>
-      <Route path="/" exact component={BurgerBuilder}></Route>
-      </Switch>
+        {routes}
       </Layout>
     </div>
   );
           }
 }
 
+const mapStateToProps = state =>{
+  return {
+    isAuthenticated:state.auth.token!==null
+  };
+
+};
 const mapDispatchToProps = dispatch =>{
   return {
     onTryAutoSignup:()=> dispatch(actions.authCheckState)
   };
 };
-export default withRouter(connect(null,mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps,mapDispatchToProps)(App));
