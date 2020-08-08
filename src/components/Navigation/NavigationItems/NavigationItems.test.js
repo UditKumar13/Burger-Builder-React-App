2f3778c84cd611cb9f64d20db36aa2389de68687
Uditkumar13 <uditkumar2401@gmail.com>
2020-08-09 03:54:01 +0530

    Jeat and enzyme documentation

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -3,7 +3,7 @@ import {configure,shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import NavigationItems from './NavigationItems'
 import NavigationItem from './NavigatonItem/NavigationItem';
-configure({adapter:new Adapter()})
+configure({adapter:new Adapter()});
 
 describe('<NavigationItems />', ()=>{
 let wrapper;
@@ -19,5 +19,10 @@ expect(wrapper.find(NavigationItem)).toHaveLength(2);
 it('should render three < NavigationItem /> elements if  authenticated',()=>{
     wrapper.setProps({isAuthenticated:true});
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
+        });
+
+it('should render three < NavigationItem /> elements if  authenticated',()=>{
+    wrapper.setProps({isAuthenticated:true});
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
         });
 });  
