16a9b38188a09c05caab28460de99d0d235211c0
Uditkumar13 <uditkumar2401@gmail.com>
2020-08-12 23:42:33 +0530

    Moving logic from action creators

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 import * as serviceWorker from './serviceWorker';
 import authReducer from './Store/reducers/auth';
-import {logoutSaga } from './Store/sagas/auth'; 
+import {watchAuth} from './Store/sagas'; 
 const composeEnhancers = process.env.NODE_ENV==='development'?window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__:null || compose;
 const rootReducer = combineReducers({
   burgerBuilder:burgerBuilderReducer,
@@ -25,7 +25,7 @@ const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(thunk,sagaMiddleware)
 ));
 
-sagaMiddleware.run(logoutSaga);
+sagaMiddleware.run(watchAuth);
 
 
 const app = (
